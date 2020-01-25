ceceaf464c5492d36d968f24f993b288c0ca39be
Ulmas Pulatov <upulatov@gmail.com>
2020-01-24 22:47:10 -0500

    replace NOOP with transparent

--- keyboards/keebio/iris/keymaps/ulmas/keymap.c
+++ keyboards/keebio/iris/keymaps/ulmas/keymap.c
@@ -21,28 +21,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC       , KC_1 , KC_2  , KC_3   , KC_4 , KC_5          , KC_6   , KC_7   , KC_8, KC_9, KC_0   , KC_MINS,
         KC_TAB        , KC_Q , KC_W  , KC_E   , KC_R , KC_T          , KC_Y   , KC_U   , KC_I, KC_O, KC_P   , KC_BSLS,
         LCTL_T(KC_GRV), KC_A , KC_S  , KC_D   , KC_F , KC_G          , KC_H   , KC_J   , KC_K, KC_L, TD(3)  , TD(4)  ,
-        KC_LSFT       , KC_Z , KC_X  , KC_C   , KC_V , KC_B          , XXXXXXX, XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT , TD(0), RSFT_T(KC_ENT),
+        KC_LSFT       , KC_Z , KC_X  , KC_C   , KC_V , KC_B          , _______, _______, KC_N, KC_M, KC_COMM, KC_DOT , TD(0), RSFT_T(KC_ENT),
         KC_LGUI       , MO(1), KC_SPC, KC_BSPC, MO(2), LALT_T(KC_DEL)
         ),
 	[1] = LAYOUT(
-        KC_TILD, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, KC_LT  , KC_GT  , KC_PSLS, KC_PAST, KC_PAST, KC_PMNS, KC_MINS,
-        BL_BRTG, BL_INC  , XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_EQL ,
-        BL_STEP, BL_DEC  , KC_LEFT, KC_RGHT, KC_UP  , KC_LCBR, KC_RCBR, KC_P4  , KC_P5  , KC_P6  , KC_PCMM, XXXXXXX,
-        LT(3, XXXXXXX), BL_TOGG, XXXXXXX, XXXXXXX, KC_DOWN, KC_LBRC, XXXXXXX, XXXXXXX, KC_RBRC, KC_P1  , KC_P2  , KC_P3, KC_PEQL, KC_PDOT,
-        XXXXXXX, KC_TRNS , KC_DEL , KC_BSPC, KC_P0  , KC_PDOT
+        KC_TILD, _______ , _______, _______, _______, KC_LT  , KC_GT  , KC_PSLS, KC_PAST, KC_PAST, KC_PMNS, KC_MINS,
+        BL_BRTG, BL_INC  , _______, _______, _______, KC_LPRN, KC_RPRN, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_EQL ,
+        BL_STEP, BL_DEC  , KC_LEFT, KC_RGHT, KC_UP  , KC_LCBR, KC_RCBR, KC_P4  , KC_P5  , KC_P6  , KC_PCMM, _______,
+        LT(3, _______), BL_TOGG, _______, _______, KC_DOWN, KC_LBRC, _______, _______, KC_RBRC, KC_P1  , KC_P2  , KC_P3, KC_PEQL, KC_PDOT,
+        _______, KC_TRNS , KC_DEL , KC_BSPC, KC_P0  , KC_PDOT
         ),
 	[2] = LAYOUT(
         KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-        RESET  , KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, KC_PEQL, KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX, XXXXXXX,
-        XXXXXXX, KC_VOLD, XXXXXXX, KC_HOME, KC_PGUP, KC_UNDS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
-        KC_MUTE, XXXXXXX, XXXXXXX, KC_END , KC_PGDN, KC_PMNS, XXXXXXX, XXXXXXX, KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, XXXXXXX
+        RESET  , KC_VOLU, _______, _______, _______, KC_PEQL, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______,
+        _______, KC_VOLD, _______, KC_HOME, KC_PGUP, KC_UNDS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
+        KC_MUTE, _______, _______, KC_END , KC_PGDN, KC_PMNS, _______, _______, KC_PPLS, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, KC_TRNS, _______
         ),
 	[3] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX, KC_WH_D,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_TRNS, XXXXXXX
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_U,
+        _______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, _______, KC_WH_D,
+        _______, _______, _______, _______, _______, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______,
+        _______, _______, _______, KC_BTN1, KC_TRNS, _______
         )
 };
