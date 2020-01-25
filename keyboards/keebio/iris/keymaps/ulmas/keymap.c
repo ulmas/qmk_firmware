e5011a51bc5ef4b4f29e0cba2f71c041508ff4f8
Ulmas Pulatov <upulatov@gmail.com>
2020-01-24 22:43:44 -0500

    replace backspace with -/_

--- keyboards/keebio/iris/keymaps/ulmas/keymap.c
+++ keyboards/keebio/iris/keymaps/ulmas/keymap.c
@@ -18,7 +18,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-        KC_GESC       , KC_1 , KC_2  , KC_3   , KC_4 , KC_5          , KC_6   , KC_7   , KC_8, KC_9, KC_0   , KC_BSPC,
+        KC_GESC       , KC_1 , KC_2  , KC_3   , KC_4 , KC_5          , KC_6   , KC_7   , KC_8, KC_9, KC_0   , KC_MINS,
         KC_TAB        , KC_Q , KC_W  , KC_E   , KC_R , KC_T          , KC_Y   , KC_U   , KC_I, KC_O, KC_P   , KC_BSLS,
         LCTL_T(KC_GRV), KC_A , KC_S  , KC_D   , KC_F , KC_G          , KC_H   , KC_J   , KC_K, KC_L, TD(3)  , TD(4)  ,
         KC_LSFT       , KC_Z , KC_X  , KC_C   , KC_V , KC_B          , XXXXXXX, XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT , TD(0), RSFT_T(KC_ENT),
