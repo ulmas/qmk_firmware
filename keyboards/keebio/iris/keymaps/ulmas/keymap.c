61e673b1f488d6c05e57ade89af54e993259bada
Ulmas Pulatov <upulatov@gmail.com>
2020-01-26 18:05:53 -0500

    replace _ENT with _BSPC

--- keyboards/keebio/iris/keymaps/ulmas/keymap.c
+++ keyboards/keebio/iris/keymaps/ulmas/keymap.c
@@ -21,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC       , KC_1 , KC_2  , KC_3   , KC_4 , KC_5          , KC_6   , KC_7   , KC_8, KC_9, KC_0   , KC_MINS,
         KC_TAB        , KC_Q , KC_W  , KC_E   , KC_R , KC_T          , KC_Y   , KC_U   , KC_I, KC_O, KC_P   , KC_BSLS,
         LCTL_T(KC_GRV), KC_A , KC_S  , KC_D   , KC_F , KC_G          , KC_H   , KC_J   , KC_K, KC_L, TD(3)  , TD(4)  ,
-        KC_LSFT       , KC_Z , KC_X  , KC_C   , KC_V , KC_B          , _______, _______, KC_N, KC_M, KC_COMM, KC_DOT , TD(0), RSFT_T(KC_ENT),
-        KC_LGUI       , MO(1), KC_SPC, KC_BSPC, MO(2), LALT_T(KC_DEL)
+        KC_LSFT       , KC_Z , KC_X  , KC_C   , KC_V , KC_B          , _______, _______, KC_N, KC_M, KC_COMM, KC_DOT , TD(0), RSFT_T(KC_BSPC),
+        KC_LGUI       , MO(1), KC_SPC, KC_ENT , MO(2), LALT_T(KC_DEL)
         ),
 	[1] = LAYOUT(
-        KC_TILD, _______ , _______, _______, _______, KC_LT  , KC_GT  , KC_PSLS, KC_PAST, KC_PAST, KC_PMNS, KC_MINS,
+        _______, _______ , _______, _______, _______, KC_LABK, KC_RABK, KC_PSLS, KC_PAST, KC_PAST, KC_PMNS, KC_MINS,
         BL_BRTG, BL_INC  , _______, _______, _______, KC_LPRN, KC_RPRN, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_EQL ,
         BL_STEP, BL_DEC  , KC_LEFT, KC_RGHT, KC_UP  , KC_LCBR, KC_RCBR, KC_P4  , KC_P5  , KC_P6  , KC_PCMM, _______,
         LT(3, _______), BL_TOGG, _______, _______, KC_DOWN, KC_LBRC, _______, _______, KC_RBRC, KC_P1  , KC_P2  , KC_P3, KC_PEQL, KC_PDOT,
